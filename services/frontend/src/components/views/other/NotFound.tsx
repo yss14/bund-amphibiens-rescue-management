5fed315e456e998524cb270f359d87b24a7be21a
Yannick Stachelscheid <stachelscheid@addbots.de>
2019-02-19 17:58:01 +0100

    [frontend] add icon and style to not found view

--- services/frontend/src/components/views/other/NotFound.tsx
+++ services/frontend/src/components/views/other/NotFound.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import img404 from '../../../images/404.png';
+import styled from 'styled-components';
+
+const CenteredWrapper = styled.div`
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+`;
 
 export const NotFound = () => (
-	<div>Not found</div>
+	<CenteredWrapper>
+		<img src={img404} alt="404 Seite nicht gefunden" width="128px" height="128px" />
+		<div style={{ paddingTop: 10, fontSize: 24, fontWeight: 'bold' }}>Seite nicht gefunden</div>
+	</CenteredWrapper>
 );
